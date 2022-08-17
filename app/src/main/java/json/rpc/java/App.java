c59682774b678d50aec62212ac58f6b4556ba123
Crystal Taylor <crystalgfeist@gmail.com>
2022-08-17 21:39:46 +1200

    basic project with dependency on github.com/arteam/simple-json-rpc.git for json-rpc java server implementation

--- app/src/main/java/json/rpc/java/App.java
+++ app/src/main/java/json/rpc/java/App.java
@@ -3,12 +3,25 @@
  */
 package json.rpc.java;
 
+import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcError;
+import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcMethod;
+import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcService;
+
+@JsonRpcService
 public class App {
+	@JsonRpcMethod
     public String getGreeting() {
         return "Hello World!";
     }
-
+	
+	@JsonRpcMethod
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
+	
+	@SuppressWarnings("serial")
+	@JsonRpcError(code = -32032, message ="Generic error")
+	public class GenericException extends Exception {
+		
+	}
 }
