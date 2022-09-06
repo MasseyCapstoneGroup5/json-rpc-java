00f0bb4c52eb28b5c7217fa785109954e0cec334
Crystal Taylor <crystalgfeist@gmail.com>
2022-09-07 00:02:19 +1200

    added a few account tests, need to implement correct error handling and mapping after determining format for requests

--- app/src/main/java/json/rpc/java/App.java
+++ app/src/main/java/json/rpc/java/App.java
@@ -11,6 +11,9 @@ import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcMethod;
 import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcService;
 import com.github.arteam.simplejsonrpc.server.JsonRpcServer;
 import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.AccountInfo;
+import com.hedera.hashgraph.sdk.AccountInfoQuery;
+import com.hedera.hashgraph.sdk.AccountUpdateTransaction;
 import com.hedera.hashgraph.sdk.HederaPreCheckStatusException;
 import com.hedera.hashgraph.sdk.HederaReceiptStatusException;
 import com.hedera.hashgraph.sdk.PrecheckStatusException;
@@ -19,7 +22,10 @@ import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.TransactionResponse;
 import com.hedera.hashgraph.sdk.PublicKey;
 import com.hedera.hashgraph.sdk.ReceiptStatusException;
+import com.hedera.hashgraph.sdk.Status;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
 import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.AccountDeleteTransaction;
 import com.hedera.hashgraph.sdk.Hbar;
 import com.hedera.hashgraph.sdk.AccountBalanceQuery;
 import com.hedera.hashgraph.sdk.AccountBalance;
@@ -43,17 +49,12 @@ import java.util.Scanner;
 public class App {
 	private PrivateKey privateKey = null;
 	private PublicKey publicKey = null;
+	private static Client client = Client.forTestnet();
 	
 	public App() {
 
 	}
 
-	
-	@JsonRpcMethod
-    public String getGreeting() {
-        return "Hello World!";
-    }
-	
     public static void main(String[] args) throws TimeoutException, PrecheckStatusException, ReceiptStatusException {
 		System.out.println("-- JSON-RPC java server running --");
     	listen(80);
@@ -93,6 +94,128 @@ public class App {
 		      System.err.println("Usage: java HttpMirror <port>");
 		    }
 	}
+	
+	// a couple of tests
+	@JsonRpcMethod
+	private static AccountInfo getAccountInfo(AccountId accountId) {
+		
+		AccountInfoQuery query = new AccountInfoQuery().setAccountId(accountId);
+		try {
+			return query.execute(client);
+		} catch (TimeoutException | PrecheckStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	@JsonRpcMethod
+	private static AccountId createAccount(PublicKey publicKey, AccountBalance accountBalance) {
+		// Generate a new key pair
+        PrivateKey newAccountPrivateKey = PrivateKey.generateED25519();
+        PublicKey newAccountPublicKey = newAccountPrivateKey.getPublicKey();
+        
+        AccountId newAccountId = null;
+        
+        //Create new account and assign the public key
+        TransactionResponse newAccount;
+		try {
+			newAccount = new AccountCreateTransaction()
+			        .setKey(newAccountPublicKey)
+			        .setInitialBalance(accountBalance.hbars)
+			        .execute(client);
+			
+			try {
+				newAccountId = newAccount.getReceipt(client).accountId;
+			} catch (ReceiptStatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PrecheckStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return newAccountId;
+	}
+	
+	@JsonRpcMethod
+	private static Status updateAccountKey(AccountId accountId, PublicKey newPublicKey, PrivateKey oldPrivateKey, PrivateKey newPrivateKey) {
+		AccountUpdateTransaction transaction = new AccountUpdateTransaction().setAccountId(accountId).setKey(newPublicKey).freezeWith(null);
+		AccountUpdateTransaction signTx = transaction.sign(oldPrivateKey).sign(newPrivateKey);
+		try {
+			TransactionResponse txResponse = signTx.execute(null);
+			try {
+				TransactionReceipt receipt = txResponse.getReceipt(null);
+				return receipt.status;
+			} catch (ReceiptStatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PrecheckStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	@JsonRpcMethod
+	private static Status updateAccountMemo(AccountId accountId, PrivateKey privateKey, String memo) {
+		AccountUpdateTransaction transaction = new AccountUpdateTransaction().setAccountId(accountId).setAccountMemo(memo).freezeWith(null);
+		AccountUpdateTransaction signTx = transaction.sign(privateKey);
+		TransactionResponse txResponse;
+		try {
+			txResponse = signTx.execute(null);
+			try {
+				TransactionReceipt receipt = txResponse.getReceipt(null);
+				return receipt.status;
+			} catch (ReceiptStatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PrecheckStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@JsonRpcMethod
+	private static TransactionReceipt deleteAccount(AccountId accountId, PrivateKey privateKey, AccountId recipientId) {
+		AccountDeleteTransaction transaction = new AccountDeleteTransaction().setAccountId(accountId).setTransferAccountId(recipientId).freezeWith(null);
+		AccountDeleteTransaction signTx = transaction.sign(privateKey);
+		try {
+			TransactionResponse txResponse = signTx.execute(null);
+			try {
+				TransactionReceipt receipt = txResponse.getReceipt(null);
+				return receipt;
+			} catch (ReceiptStatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PrecheckStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
 
 	private static void hederaTest() throws TimeoutException, PrecheckStatusException, ReceiptStatusException {  
         //Grab your Hedera testnet account ID and private key
